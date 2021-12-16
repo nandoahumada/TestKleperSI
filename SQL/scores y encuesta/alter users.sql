fd01a41488971a537c9af830ef60cb6450db2378
nandoahumada <41706580+nandoahumada@users.noreply.github.com>
2021-12-16 09:04:11 -0300

    sqls

---
+++ SQL/scores y encuesta/alter users.sql
@@ -0,0 +1,19 @@
+alter table users
+add column comp_name varchar(20)
+after name;
+
+alter table users
+add column area varchar(20)
+after comp_name;
+
+alter table users
+add column phone varchar(20)
+after password;
+
+alter table users
+add column workers_num int unsigned
+after phone;
+
+alter table users
+add column pcs_num int unsigned
+after workers_num;
