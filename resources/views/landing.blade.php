77ae1b953904e979bd67078f6b12519c6095a7a6
nandoahumada <41706580+nandoahumada@users.noreply.github.com>
2022-01-17 13:13:34 -0300

    cambios landing

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -6,25 +6,54 @@
 <div class="container">
     <div class="container">
       <h5 class="card-title">Cyber Risk Assessment</h5>
-      <p class="card-text"> Bienvenidos a ONION 3! ¡La más innovadora solución de administración de ciber riesgo en tu ambiente de negocios!
-        A continuación, entrarás en un assesstment para evaluar tus ciber defensas, determinar el grado vulnerabilidad de tu empresa y compromiso de afección de tu ecosistema de negocios frente a ciberataques. Esta evaluación se realiza de acuerdo con los controles establecidos por la CIS (Center for Internet Security), una de las comunidades de ciberseguridad más respetadas y reconocidas del mundo, y adecuados por la empresa Kepler, con 20 años de trayectoria y altamente comprometida con la innovación en ciberseguridad. 
-        Los resultados de este assessment serán procesados por Kepler con el fin de entregar un estado actual de defensas, capacidad de respuesta y vulnerabilidades del evaluado. 
-        La información de este assesstment generará un reporte de riesgo cibernético, que permitirá la gestión y administración del riesgo en tu empresa y ecosistema de negocios.</p>
+      <p class="card-text"> Bienvenidos a ONION 3! ¡La más innovadora solución de administración de ciber riesgo en tu ecosistema de negocios! 
+
+        <li class="list-group-item">  En el presente assessment será denominado como Proveedor quien responda las preguntas aquí contenidas con el objeto de evaluar sus ciber defensas, 
+        determinar el grado vulnerabilidad de la empresa y compromiso de afección del ecosistema de negocios frente a ciberataques.</li>
+        
+        <li class="list-group-item">  Se denominará Master Client a quien requirió del Proveedor la contratación y ejecución del assessment, y, por su parte, 
+        la empresa Kepler será la encargada de recibir y analizar las respuestas del Proveedor y entregar los resultados correspondientes.</li>
+        
+        <li class="list-group-item">  Este assessmente se realiza de acuerdo con los controles establecidos por la CIS (Center for Internet Security), 
+        una de las comunidades de ciberseguridad más respetadas y reconocidas del mundo y adecuados por la empresa Kepler, la que cuenta con 20 años de trayectoria y 
+        está altamente comprometida con la innovación en ciberseguridad.</li> 
+
+        <li class="list-group-item">Los resultados de este assessment serán procesados por Kepler con el fin de entregar un estado actual de defensas, 
+        capacidad de respuesta y vulnerabilidades del Proveedor evaluado.</li> 
+
+        <li class="list-group-item">La información resultante deeste assesstment generará, por parte de Kepler, un reporte de riesgo cibernético, 
+        que permitirá la gestión y administración del riesgo en la empresa del Proveedor y ecosistema de negocios.</li>
+</p>
     </div>
     <ul class="list-group list-group-flush">
         <li class="list-group-item"> Consideraciones importantes:</li>
-      <li class="list-group-item">1.	Este assesstment contiene preguntas relacionadas con los ámbitos técnicos de hardware y software de tu empresa destinados a la ciberseguridad. Estas preguntas, están basadas en los 18 controles de la CIS, divididos en 3 grupos y enfocados en IG1 (Implementation Group 1).</li>
-      <li class="list-group-item">2.	Estas preguntas deberán ser respondidas por la o las personas que posean el conocimiento técnico para realizarlo, en caso de no conocer alguna de las respuestas puedes contactarnos para solicitar el apoyo requerido.</li>
-      <li class="list-group-item">3.  El contenido de las respuestas de este assessment tiene carácter de confidencial y no serán divulgadas ni compartidas con terceros, sin embargo, la información de clasificación y puntajes podrán ser compartidas para establecer criterios generales de protección del ecosistema de negocios y planes de mejora siempre con expresa autorización de los titulares de la información.</li>
-      <li class="list-group-item">4.	Con el fin de mejorar el ecosistema nacional y la concientización en ciberseguridad, Los datos recogidos a través de la página web podrán ser conservados y tratados con fines estadísticos, siempre y cuando sea imposible la identificación de sus titulares. Los datos recabados a través del medio web no se comunicarán a terceros, salvo que:</li>
-      <li class="list-group-item">4-A.	Dicha comunicación sea necesaria para la prestación del servicio solicitado, y siempre que ese tercero cumpla con las garantías necesarias sobre protección de datos.</li>
-      <li class="list-group-item">4-B.	Se trate de datos estadísticos, agregados y anónimos donde no sea posible la identificación directa o indirecta de los titulares de dichos datos.</li>
-      <li class="list-group-item">4-C.	Se realice en cumplimiento de un mandato legal o un requerimiento judicial o en los casos en que sea necesario para garantizar la seguridad nacional o el interés público.</li>
-      <li class="list-group-item">5.  El tiempo de respuesta es aproximado de 100 minutos y debe ser contestado de una sola vez.</li>
+      <li class="list-group-item">1.	Este assesstment contiene preguntas relacionadas con los ámbitos técnicos de hardware y software de la empresa del Proveedor  
+        destinados a la ciberseguridad. Dichas  preguntas, están basadas en los 18 controles de la CIS, divididos en 3 grupos y enfocados en IG1 (Implementation Group 1).</li>
+      <li class="list-group-item">2.	Las preguntas deberán ser respondidas por la o las personas designadas por el Proveedor y que posean el conocimiento técnico para 
+        responderlas. Si por cualquier motivo o circunstancia la o las personas designadas no pudieran  responder  alguna de las preguntas contenidas en el assessment, el 
+        Proveedor podrá contactar  a Kepler  para solicitar el apoyo requerido.</li>
+      <li class="list-group-item">3.  El contenido de las respuestas de este assessment tendrán  el  carácter de confidencial y no será divulgado ni compartido con terceros.
+        Sin perjuicio de lo anterior, la información de clasificación y puntajes obtenidos de la realización del assessment podrán ser compartidas 
+        con el Master Client, sin necesidad de autorización por parte del Proveedor. La divulgación de la información tiene por objeto el establecimiento de criterios 
+        generales de protección del ecosistema de negocios y planes de mejora por parte del Proveedor.</li>
+      <li class="list-group-item">4.	Con el fin de mejorar el ecosistema nacional y la concientización en ciberseguridad, los datos recogidos a través de la página web 
+        podrán ser conservados y tratados con fines estadísticos, siempre y cuando sea imposible la identificación de sus titulares. Los datos recabados a 
+        través del medio web no se comunicarán a terceros, salvo que:</li>
+      <li class="list-group-item">4-A.	Dicha comunicación sea necesaria para la prestación del servicio solicitado, y siempre que ese tercero cumpla con las garantías 
+        necesarias sobre protección de datos.</li>
+      <li class="list-group-item">4-B.	Se trate de datos estadísticos, agregados y anónimos donde no sea posible la identificación directa o indirecta de los 
+        titulares de dichos datos.</li>
+      <li class="list-group-item">4-C.	Se realice en cumplimiento de un mandato legal o un requerimiento judicial o en los casos en que sea 
+        necesario para garantizar la seguridad nacional o el interés público.</li>
+      <li class="list-group-item">5.  El tiempo de respuesta es de aproximadamente 100 minutos y deberá ser contestado de una sola vez. Tenga en consideración que este 
+        formulario no almacena las respuestas en ningún momento, sólo las envía a una base de datos segura una vez contestado completo y 
+        enviado por el Proveedor al pulsar el botón ENVIAR.</li>
       <li class="list-group-item">¡Éxito en tu assesstment de ciber riesgo y gracias por confiar en nosotros!</li>
           
-    <li class="list-group-item"> Si no puedes o no tienes el tiempo de contestar este correo, podemos hacerlo por ti. Enviaremos un ingeniero especialista en ciberseguridad para realizar un assessment (tiene un costo adicional) 
-      Recuerda además que debes siempre contar con un EDR, Firewall y políticas de parchado continuo. Si necesitas ayuda con esto, puedes ponerte en contacto con nosotros escribiendo a nuestro <a href="mailto:redteam@kepler.cl" target="_blank" class="btn btn-primary">REDTEAM</a>.</li>
+    <li class="list-group-item">•	Si no puedes o no tienes el tiempo de contestar este Cyber Risk Assessment, podemos hacerlo por ti. Enviaremos un ingeniero especialista en 
+      ciberseguridad para realizar un assessment (el cual tendrá un costo adicional) Recuerda además que debes siempre contar con un EDR, Firewall y políticas de parchado 
+      continuo. Si necesitas ayuda con esto, puedes ponerte en contacto con nosotros escribiendo a 
+      nuestro <a href="mailto:redteam@kepler.cl" target="_blank" class="btn btn-primary">REDTEAM</a>.</li>
       <li class="list-group-item">Equipo ONION 3 </li> 
       
       <li class="list-group-item">  Haciendo click en el boton Iniciar, se darán por leídos y aceptados los términos y condiciones descritos”</li>
