e10ddc0ebd957bd61d10c20dfc65de012ff1851a
nandoahumada <41706580+nandoahumada@users.noreply.github.com>
2022-01-31 13:50:36 -0300

    sc
    sc

--- SQL/scores y encuesta/ayuda selects.sql
+++ SQL/scores y encuesta/ayuda selects.sql
@@ -1,13 +1,15 @@
-select * from selectanswersbyuser
-where id = 9
-AND ENCUESTADO = 'Mandiolo';
+select * from SelectAnswersByUser
+where id = 4
+AND ENCUESTADO = 'danitza';
 
-SELECT SUM(PUNTAJE) FROM selectanswersbyuser
-WHERE ID = 11
-AND ENCUESTADO = 'Mandiolo';
+  SELECT SUM(PUNTAJE) FROM SelectAnswersByUser
+  WHERE ID = 4
+  AND ENCUESTADO = 'danitza';
 
-select * from scores
-where real_section = 14;
+select qu.id,qu.real_section,qu.content,sc.id,sc.options,sc.value from questions qu
+inner join scores sc
+on qu.id = sc.question_id
+where sc.real_section in (4,9,11);
 
 select * from
 questions 
--- SQL/scores y encuesta/scores fix.sql
+++ SQL/scores y encuesta/scores fix.sql
@@ -1,10 +1,12 @@
-update scores set value = 25 where id = 293;
-update scores set value = 25 where id = 297;
-update scores set value = 25 where id = 299;
+update scores set value = 25 where id = 294;
+update scores set value = 25 where id = 298;
 update scores set value = 25 where id = 300;
-update scores set value = 25 where id = 304;
+update scores set value = 25 where id = 301;
+update scores set value = 25 where id = 305;
 
-update scores set value = 20 where id = 238;
-update scores set value = 15 where id = 243;
-update scores set value = 20 where id = 248;
-update scores set value = 15 where id = 253;
+update scores set value = 20 where id = 239;
+update scores set value = 15 where id = 242;
+update scores set value = 20 where id = 249;
+update scores set value = 20 where id = 252;
+
+
